28ddb61b94c1c567d6a8ced321f64c520d3f20f3
Anh Vu <anh@GS-ITs-MacBook-Pro.local>
2019-10-03 11:20:07 -0400

    initial working version

--- Angular/WebGUI/README.md
+++ Angular/WebGUI/README.md
@@ -1,6 +1,6 @@
-# WebGUI
+# Angularinterface
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.0.
 
 ## Development server
 
--- Angular/WebGUI/src/app/app.module.ts
+++ Angular/WebGUI/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import this to read/Observe data.json
+import { HttpClientModule } from '@angular/common/http';
+// import delete icon
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import this to help with Form. How?
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-component/app.component';
+import { AddComponent } from './components/add/add.component';
+import { SearchComponent } from './components/search/search.component';
+import { ListComponent } from './components/list/list.component';
+import { SpreadComponent } from './components/spread/spread.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    SearchComponent,
+    ListComponent,
+    SpreadComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, HttpClientModule, FontAwesomeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
