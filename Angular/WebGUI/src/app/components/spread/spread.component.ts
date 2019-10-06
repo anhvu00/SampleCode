7730a36b7f471552bc4e10eb593778169b5de2f7
Anh Vu <anh@GS-ITs-MacBook-Pro.local>
2019-10-05 20:17:46 -0400

    Edit spread works

--- Angular/WebGUI/src/app/components/spread/spread.component.ts
+++ Angular/WebGUI/src/app/components/spread/spread.component.ts
@@ -7,4 +7,15 @@ import { Component, Input } from '@angular/core';
 export class SpreadComponent {
   @Input() spreadList;
 
+  // handle add
+  handleNewSpread() {
+    // local var to manipulate form fields, like a json.
+    console.log(this.spreadList);
+  }
+  updateList(id: number, property: string, event: any) {
+    const editField = event.target.textContent;
+    this.spreadList[id][property] = editField;
+    console.log(editField);
+  }
+
 }
