5c04ceee0c68ad24d1d259aa4c5e738281fa8877
anh_sme <anh@strategicmissionelements.com>
2023-12-06 17:51:00 -0500

    Add Prometheus micrometer to the consumer

--- KafkaDemo1/kafkaConsumer/src/main/java/com/kyron/sample/kafkaConsumer/KafkaConsumerService.java
+++ KafkaDemo1/kafkaConsumer/src/main/java/com/kyron/sample/kafkaConsumer/KafkaConsumerService.java
@@ -1,11 +1,14 @@
 package com.kyron.sample.kafkaConsumer;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 import jakarta.annotation.PostConstruct;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -14,6 +17,12 @@ import java.util.Properties;
 
 @Service
 public class KafkaConsumerService {
+    private final MeterRegistry meterRegistry;
+
+    @Autowired
+    public KafkaConsumerService(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
 
     /*
      * Uncomment this and comment out the @PostConstruct method
@@ -27,8 +36,8 @@ public class KafkaConsumerService {
 
     /**
      * Use the KafkaConsumer API directly.
-     * This attempts to delay the consume, just 1 every 4 secs = unsuccessful
-     *
+     * This attempts to delay the consume, just 1 every 5 secs = unsuccessful
+     */
     @PostConstruct
     public void consumeWithKafkaConsumerApi() {
         Properties properties = new Properties();
@@ -40,16 +49,29 @@ public class KafkaConsumerService {
         Consumer<String, String> consumer = new KafkaConsumer<>(properties);
         consumer.subscribe(Collections.singletonList("anh-topic"));
 
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(4000));
-            records.forEach(record -> System.out.println("Received by api: " + record.value()));
+        long startTime = System.currentTimeMillis();
+        long durationInMinutes = 3;
+
+        while (System.currentTimeMillis() - startTime < durationInMinutes * 60 * 1000) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(5000));
+            records.forEach(record -> {
+                System.out.println("Received by api: " + record.value());
+                TopicPartition topicPartition = new TopicPartition(record.topic(), record.partition());
+                long lag = record.offset() - consumer.position(topicPartition);
+                System.out.println("   Lag for partition " + record.partition() + ": " + lag);
+
+                // Expose lag metrics to Prometheus
+                meterRegistry.counter("kafka_lag", Tags.of("topic", record.topic(), "partition", String.valueOf(record.partition())))
+                        .increment(lag);
+            });
         }
     }
-    */
+
 
     /**
      * Modified version to force slower consumption
-     */
+     * Don't know how to get the consumer to calc lag here.
+     *
     @KafkaListener(topics = "anh-topic")
     public void consume(String message) {
         System.out.println("Received from listener: " + message);
@@ -59,5 +81,6 @@ public class KafkaConsumerService {
             e.printStackTrace();
         }
     }
+    */
 }
 
